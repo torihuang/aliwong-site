a32f5e2492f31b8ea3ed6bdbfe15777ca123ce51
torihuang <victoriahuang20@gmail.com>
2016-06-06 11:23:55 -0500

    Add routes and scrolling

--- app/js/controllers/TourController.js
+++ app/js/controllers/TourController.js
@@ -1,7 +1,41 @@
 'use strict';
 
-aliwongApp.controller('TourController', function TourController($scope, tourData) {
+aliwongApp.controller('TourController', function TourController($scope, tourData, $window, $anchorScroll, $location) {
   $scope.test = 'tour controller worked!';
   $scope.tours = tourData.getAllTours();
+  $scope.tourIdShow = -1;
+
+  $scope.assignTourId = function(tourId) {
+    if (this.tourIdShow == tourId) {
+      this.tourIdShow = -1;
+    } else {
+      this.tourIdShow = tourId;
+    }
+  };
+
+  $scope.openTour = function(tourId) {
+    if (this.tourIdShow == tourId) {
+      console.log('here')
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  $scope.goToNetflix = function() {
+    $window.open('https://www.netflix.com', '_blank');
+  }
+
+  $scope.gotoAnchor = function(x) {
+    if ($location.hash() !== x) {
+      // set the $location.hash to `newHash` and
+      // $anchorScroll will automatically scroll to it
+      $location.hash(x);
+    } else {
+      // call $anchorScroll() explicitly,
+      // since $location.hash hasn't changed
+      $anchorScroll();
+    }
+  };
   // $scope.tours = $scope.toursArray[0];
 })
--- scripts/web-server.js
+++ scripts/web-server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 // static serve pages in path without calling them at all
 // install all files in app directory without processing them at all, just pass out as they are
 app.use(express.static(rootPath + '/app'));
+app.get('*', function(req, res) {res.sendFile(rootPath + '/app/index.html');});
 
 // app.get('/data/tour', tours.getAll);
 
